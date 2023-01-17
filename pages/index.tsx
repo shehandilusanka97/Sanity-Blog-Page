0495028348c6efb505fceee6f40442421ec498bf
shehandilusanka97 <120352242+shehandilusankacristalcode@users.noreply.github.com>
2023-01-17 13:22:23 +0530

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,40 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Header from '../components/Header'
-
-
+import type { NextPage } from "next";
+import Head from "next/head";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>CK's Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-     <Header/>
-    </div>
-  )
-}
+      <Header />
+      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+        <div className="px-10 space-y-5 ">
+          <h1 className="text-6xl max-w-xl font-serif ">
+            <span className="underline decoration-black decoration-4">
+              Medium
+            </span>{" "}
+            is for Acadamic purpose. Peace Homies ✌️
+          </h1>
+          <h2>
+            It's easy and free to post your thinking on any topic and connect
+            with millions of readers
+          </h2>
+        </div>
+        <img
+          className="hidden md:inline-flex h-32 lg:h-full"
+          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+          alt=""
+        />
+      </div>
 
-export default Home
+      {/* post  */}
+      
+
+    </div>
+  );
+};
+
+export default Home;
