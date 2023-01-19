149ed58285a0ef0db9376f6d2c4aba5002ab3463
shehandilusanka97 <120352242+shehandilusankacristalcode@users.noreply.github.com>
2023-01-19 10:19:08 +0530

    commit

--- cksblogs-sanity/schemas/post.js
+++ cksblogs-sanity/schemas/post.js
@@ -8,6 +8,7 @@ export default defineType({
     defineField({
       name: 'title',
       title: 'Title',
+      description:'Keep titles short!',
       type: 'string',
     }),
     defineField({
--- sanity.js
+++ sanity.js
@@ -5,6 +5,12 @@ import{
 } from 'next-sanity';
 
 export const config={
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    apiVersion:"v1",
 
-    data
+    useCdn: process.env.NODE_ENV ==="production",
 }
+export const sanityClient = createClient(config);
+// export const urlFor =(source)=> createImageUrlBuilder(config).image(source);
+// export const useCurrentUser =createCurrentUserHook(config);
